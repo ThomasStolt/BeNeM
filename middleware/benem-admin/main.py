4e0b1c5899e1d27a9e8069cb4a9f0e9d95bd705d
Thomas Stolt <thomas.stolt@gmail.com>
2026-03-30 20:29:47 +0200

    fix: add .dockerignore, non-root user, split dev requirements

--- middleware/benem-admin/main.py
+++ middleware/benem-admin/main.py
@@ -1,11 +1,11 @@
-VERSION = "1.0.0"
-
 import os
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from dotenv import load_dotenv
 
 load_dotenv()
+
+VERSION = "1.0.0"
 
 app = FastAPI(docs_url=None, redoc_url=None)
 templates = Jinja2Templates(directory="templates")
